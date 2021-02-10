4a20419a7b2ee84e736ae212fe4695601f526ac6
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-02-10 11:41:37 +0100

    page sections, images, responsive layout,

---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      sans: ["Raleway", "sans-serif"],
+    },
+    extend: {
+      backgroundImage: (theme) => ({
+        "hero-img": "url('/img/hero-1.jpg')",
+        "couple-img": "url('/img/hero-img.jpg')",
+        "scotland-img": "url('/img/scotland.jpg')",
+        "prague-img": "url('/img/prague.jpg')",
+        "london-img": "url('/img/london.jpg')",
+        "munich-img": "url('/img/munich.jpg')",
+        "budapest-img": "url('/img/budapest.jpg')",
+      }),
+      spacing: {
+        144: "36rem",
+      },
+      colors: {
+        "theme-yellow": "#FAED26",
+        "theme-purple": "#46344E",
+        "theme-grey": "#5A5560",
+        "theme-rose": "#9D8D8F",
+        "theme-orange": "#9B786F",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
