54da9efe4f853307d0d0c9f018c82bd29212f0a3
Christine <50479077+christinebogdan@users.noreply.github.com>
2021-02-11 21:29:00 +0100

    fix image paths

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,13 +7,13 @@ module.exports = {
     },
     extend: {
       backgroundImage: (theme) => ({
-        "hero-img": "url('/img/hero-1.jpg')",
-        "couple-img": "url('/img/hero-img.jpg')",
-        "scotland-img": "url('/img/scotland.jpg')",
-        "prague-img": "url('/img/prague.jpg')",
-        "london-img": "url('/img/london.jpg')",
-        "munich-img": "url('/img/munich.jpg')",
-        "budapest-img": "url('/img/budapest.jpg')",
+        "hero-img": "url('../img/hero-1.jpg')",
+        "couple-img": "url('../img/hero-img.jpg')",
+        "scotland-img": "url('../img/scotland.jpg')",
+        "prague-img": "url('../img/prague.jpg')",
+        "london-img": "url('../img/london.jpg')",
+        "munich-img": "url('../img/munich.jpg')",
+        "budapest-img": "url('../img/budapest.jpg')",
       }),
       spacing: {
         144: "36rem",
